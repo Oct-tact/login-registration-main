2887dea7b133fdbc9b6ec016a9fbe0350d5cd4ab
anushka mathur <135579850+Oct-tact@users.noreply.github.com>
2024-01-11 17:58:17 +0530

    Add files via upload

---
+++ hooks.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect  } from 'react' 
+
+interface User {
+  id: number,
+  username: string,
+}
+
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+  const [users, setUsers] = useState<User[] | null>(null)
+
+
+  useEffect(() => {
+    console.log('mounting')
+    console.log('Users: ', users)
+
+    return () => console.log('unmounting')
+  }, [users])
+
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+    </div>
+  )
+}
+
+export default App
